1c82dbcacee39bb061f71bfd700128d30a8d434d
Emilija Ceple <164988569+EmilijaCeple@users.noreply.github.com>
2025-02-14 15:39:34 +0200

    Add files via upload

---
+++ appendix.sql
@@ -0,0 +1,20 @@
+WITH
+  aggregated_sales AS (
+  SELECT
+    location_id,
+    promotion,
+    ROUND(SUM(sales_in_thousands), 2) AS sales
+  FROM
+    `turing_data_analytics.wa_marketing_campaign`
+  GROUP BY
+    location_id,
+    promotion)
+SELECT
+  Promotion,
+  COUNT(location_id) AS Sample_size,
+  ROUND(AVG(aggregated_sales.sales), 2) AS Mean,
+  ROUND(STDDEV(aggregated_sales.sales), 2) AS Standard_deviation
+FROM
+  aggregated_sales
+GROUP BY
+  promotion
